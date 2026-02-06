41df99e8690abddd23e6cafe483e905a3061f8bb
1bali1 <ispanbali6@gmail.com>
2026-02-06 09:59:23 +0100

    Renamed functions, added profile by player name

--- pyro/Scripts/Bot/Cogs/Profile.cs
+++ pyro/Scripts/Bot/Cogs/Profile.cs
@@ -18,7 +18,7 @@ namespace pyro.Scripts.Bot.Cogs
         }
 
         [SlashCommand("discord", "View someone's Pyro profile by their Discord user ID!")]
-        public async Task GetProfile(Discord.WebSocket.SocketUser? user = null)
+        public async Task ProfileByDiscordId(Discord.WebSocket.SocketUser? user = null)
         {
             await Context.Interaction.DeferAsync();
 
@@ -34,6 +34,39 @@ namespace pyro.Scripts.Bot.Cogs
                 await Context.Interaction.FollowupAsync(embed: embed.Build());
                 return;
             }
+
+            string favCharacter = account.profiles["athena"]["stats"]["attributes"]["favorite_character"].AsString;
+
+            if (string.IsNullOrEmpty(favCharacter)) { favCharacter = iconUrl.Replace("{characterId}", "CID_A_402_Athena_Commando_F_RebirthFresh"); }
+            else { favCharacter = iconUrl.Replace("{characterId}", favCharacter); }
+
+            embed.WithThumbnailUrl(favCharacter);
+            
+            embed.AddField(new EmbedFieldBuilder { Name = "Name", Value = account.username });
+            embed.AddField(new EmbedFieldBuilder { Name = "V-Bucks", Value = account.vbucks });
+            embed.AddField(new EmbedFieldBuilder { Name = "Friends", Value = 0 }); // TODO: firends model + fix 
+            embed.AddField(new EmbedFieldBuilder { Name = "Is banned", Value = account.isBanned ? "Yes" : "No" });
+            embed.AddField(new EmbedFieldBuilder { Name = "Creation date", Value = account.createdOn });
+
+            await Context.Interaction.FollowupAsync(embed: embed.Build());
+        }
+
+        [SlashCommand("name", "View someone's profile by their player name!")]
+        public async Task ProfileByName(string username)
+        {
+            await Context.Interaction.DeferAsync();
+
+            var embed = Utils.Utils.CreateEmbed("Profile", $"{username}'s Pyro account", "Profile", Context.User);
+            var account = await _database.users.Find(p => p.username == username).FirstOrDefaultAsync();
+
+            if (account == null)
+            {
+                embed.Description = $"There's no account named {username}!";
+                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                return;
+            }
+
+            embed.Description = $"<@{account.discordUserId}>'s Pyro account";
 
             string favCharacter = account.profiles["athena"]["stats"]["attributes"]["favorite_character"].AsString;
 
