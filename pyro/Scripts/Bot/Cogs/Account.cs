06aa50616262d2ae424e8dd0645ccc96f35129cb
1bali1 <ispanbali6@gmail.com>
2026-02-20 20:01:52 +0100

    Add and remove vbuck command to bot

--- pyro/Scripts/Bot/Cogs/Account.cs
+++ pyro/Scripts/Bot/Cogs/Account.cs
@@ -19,31 +19,31 @@ namespace pyro.Scripts.Bot.Cogs
         [SlashCommand("create", "Create a Pyro account!")]
         public async Task AccountCreate()
         {
-            await Context.Interaction.RespondWithModalAsync<CreateAccountModal>("accountCreateModal");
+            await RespondWithModalAsync<CreateAccountModal>("accountCreateModal");
         }
 
         [ModalInteraction("accountCreateModal", ignoreGroupNames: true)]
         public async Task HandleAccountModal(CreateAccountModal modal)
         {
-            await Context.Interaction.DeferAsync();
+            await DeferAsync();
 
             string response = await _database.CreateUser(modal.username, modal.email, modal.password, Context.User.Id);
             var embed = Utils.Utils.CreateEmbed("Registration", response, "Account", Context.User);
 
-            await Context.Interaction.FollowupAsync(embed: embed.Build());
+            await FollowupAsync(embed: embed.Build());
         }
 
         [SlashCommand("ban", "Ban a Pyro account!")]
         public async Task AccountBan(Discord.WebSocket.SocketUser user)
         {
-            await Context.Interaction.DeferAsync();
+            await DeferAsync();
 
             var embed = Utils.Utils.CreateEmbed("User ban", "You have successfully banned the user!", "Account", Context.User);
 
             if (!Utils.Utils.owners.Contains(Context.User.Id))
             {
                 embed.Description = "You don't have enough permissions to use this command!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
@@ -52,26 +52,26 @@ namespace pyro.Scripts.Bot.Cogs
             if (account == null)
             {
                 embed.Description = $"{user.GlobalName} doesn't have a registered account!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
             await _database.users.UpdateOneAsync(p => p.discordUserId == user.Id, Builders<User>.Update.Set(u => u.isBanned, true));
 
-            await Context.Interaction.FollowupAsync(embed: embed.Build());
+            await FollowupAsync(embed: embed.Build());
         }
 
         [SlashCommand("unban", "Unban a Pyro account!")]
         public async Task AccountUnban(Discord.WebSocket.SocketUser user)
         {
-            await Context.Interaction.DeferAsync();
+            await DeferAsync();
 
             var embed = Utils.Utils.CreateEmbed("Unban user", "You have successfully unbanned the account!", "Account", Context.User);
 
             if (!Utils.Utils.owners.Contains(Context.User.Id))
             {
                 embed.Description = "You don't have enough permissions to use this command!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
@@ -80,26 +80,26 @@ namespace pyro.Scripts.Bot.Cogs
             if (account == null)
             {
                 embed.Description = $"{user.GlobalName} doesn't have a registered account!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
             await _database.users.UpdateOneAsync(p => p.discordUserId == user.Id, Builders<User>.Update.Set(u => u.isBanned, false));
 
-            await Context.Interaction.FollowupAsync(embed: embed.Build());
+            await FollowupAsync(embed: embed.Build());
         }
 
         [SlashCommand("full-locker", "Give someone a full locker!")]
         public async Task GiveFullLocker(Discord.WebSocket.SocketUser user)
         {
-            await Context.Interaction.DeferAsync();
+            await DeferAsync();
 
             var embed = Utils.Utils.CreateEmbed("Give full locker", $"You have successfully given {user.Mention} a full locker!", "Account", Context.User);
 
             if (!Utils.Utils.owners.Contains(Context.User.Id))
             {
                 embed.Description = "You don't have enough permissions to use this command!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
@@ -108,7 +108,7 @@ namespace pyro.Scripts.Bot.Cogs
             if (account == null)
             {
                 embed.Description = $"{user.GlobalName} doesn't have a registered account!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
@@ -123,7 +123,7 @@ namespace pyro.Scripts.Bot.Cogs
 
             await _database.users.UpdateOneAsync(p => p.discordUserId == user.Id, Builders<User>.Update.Set("profiles.athena.items", profile["items"]));
 
-            await Context.Interaction.FollowupAsync(embed: embed.Build());
+            await FollowupAsync(embed: embed.Build());
 
         }
 
@@ -137,24 +137,24 @@ namespace pyro.Scripts.Bot.Cogs
             if (account == null || account.isBanned)
             {
                 embed.Description = "You don't have a registered Pyro account, or you are banned!";
-                await Context.Interaction.RespondAsync(embed: embed.Build());
+                await RespondAsync(embed: embed.Build());
                 return;
             }
             
-            await Context.Interaction.RespondWithModalAsync<DeleteAccountModal>("deleteAccountModal");
+            await RespondWithModalAsync<DeleteAccountModal>("deleteAccountModal");
         }
 
         [ModalInteraction("deleteAccountModal", ignoreGroupNames: true)]
         public async Task HandleAccountDelete(DeleteAccountModal modal)
         {
-            await Context.Interaction.DeferAsync();
+            await DeferAsync();
 
             var embed = Utils.Utils.CreateEmbed("Delete account", "You have successfully deleted your account!", "Account", Context.User);
 
             if (modal.confirmation != "CONFIRM")
             {
                 embed.Description = "Account deletion cancelled!";
-                await Context.Interaction.FollowupAsync(embed: embed.Build());
+                await FollowupAsync(embed: embed.Build());
                 return;
             }
 
@@ -163,8 +163,64 @@ namespace pyro.Scripts.Bot.Cogs
             if (account != null && !account.isBanned) await _database.users.DeleteOneAsync(p => p.discordUserId == Context.User.Id);
             else embed.Description = "There was an error while trying to delete your account!";
 
-            await Context.Interaction.FollowupAsync(embed: embed.Build());
+            await FollowupAsync(embed: embed.Build());
             return;
+        }
+
+        [SlashCommand("add-vbucks", description: "Add vbucks to a user's balance!")]
+        public async Task AccountAddVbucks(Discord.WebSocket.SocketUser user, [MaxValue(99999)] [MinValue(1)] int amount)
+        {
+            await DeferAsync();
+
+            var embed = Utils.Utils.CreateEmbed("Increase vbucks", $"You have successfully increased {user.Mention}'s vbuck balance by {amount}!", "Account", Context.User);
+
+            if (!Utils.Utils.owners.Contains(Context.User.Id))
+            {
+                embed.Description = "You don't have enough permissions to use this command!";
+                await FollowupAsync(embed: embed.Build());
+                return;
+            }
+
+            var account = await _database.users.Find(p => p.discordUserId == user.Id).FirstOrDefaultAsync();
+
+            if (account == null)
+            {
+                embed.Description = $"{user.GlobalName} doesn't have a registered account!";
+                await FollowupAsync(embed: embed.Build());
+                return;
+            }
+
+            await _database.users.UpdateOneAsync(p => p.discordUserId == user.Id, Builders<User>.Update.Inc("vbucks", amount));
+            
+            await FollowupAsync(embed: embed.Build()); 
+        }
+
+        [SlashCommand("remove-vbucks", description: "Remove vbucks from a user's balance!")]
+        public async Task AccountRemoveVbucks(Discord.WebSocket.SocketUser user, [MaxValue(99999)] [MinValue(1)] int amount)
+        {
+            await DeferAsync();
+
+            var embed = Utils.Utils.CreateEmbed("Decrease vbucks", $"You have successfully decreased {user.Mention}'s vbuck balance by {amount}!", "Account", Context.User);
+
+            if (!Utils.Utils.owners.Contains(Context.User.Id))
+            {
+                embed.Description = "You don't have enough permissions to use this command!";
+                await FollowupAsync(embed: embed.Build());
+                return;
+            }
+
+            var account = await _database.users.Find(p => p.discordUserId == user.Id).FirstOrDefaultAsync();
+
+            if (account == null)
+            {
+                embed.Description = $"{user.GlobalName} doesn't have a registered account!";
+                await FollowupAsync(embed: embed.Build());
+                return;
+            }
+
+            await _database.users.UpdateOneAsync(p => p.discordUserId == user.Id, Builders<User>.Update.Inc("vbucks", -amount));
+
+            await FollowupAsync(embed: embed.Build()); 
         }
     }
 
